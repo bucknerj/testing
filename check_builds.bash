dc50941803aa863efeec1ef617a2d68835b502de
Josh Buckner <bucknerj@umich.edu>
2026-02-03 09:18:00 -0500

    print date of build

--- check_builds.bash
+++ check_builds.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-for outfile in $(ls install-*/*-build-*.out); do
+for outfile in $(ls install-*/*.build-*.out); do
   status=PASS
   grep -q fail $outfile
   if [[ "$?" -eq "0" ]]; then
     status=FAIL
   fi
+  build_date=$(awk '/^BEGIN BUILD SCRIPT/ {
+    print substr($0, 20);
+    exit }' $outfile)
   build_name=$(awk '/^DETECTED: build name/ {
     start = index($0, "->");
     end = index($0, "<-");
     print substr($0, start + 2, end - start - 2);
     exit }' $outfile)
-  echo "BUILD $build_name FILE $outfile STATUS $status"
+  echo "BUILD $build_name DATE $build_date FILE $outfile STATUS $status"
 done
